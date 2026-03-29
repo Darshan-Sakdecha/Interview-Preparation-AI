89bd6593616a3d0ed464cbb5d0be2ab07795d882
darshansakdecha <darshansakdecha@gmail.com>
2026-03-29 19:31:17 +0530

    fix: update CORS configuration to allow multiple origins

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,12 +6,13 @@ import { interviewRouter } from "./routes/interview.routes.js";
 
 const app = express();
 
-app.use(
-    cors({
-        origin: "http://localhost:5173",
-        credentials: true,
-    })
-);
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "https://interview-preparation-ai-frontend.onrender.com"
+    ],
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(cookieParser());
